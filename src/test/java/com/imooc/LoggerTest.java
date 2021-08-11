af2cf511d75b09f3b0298fc7efe1fba3fa5c3405
LiverpoolDaoZhou <LiverpoolDaoZhou@gmail.com>
2021-08-12 00:50:40 +0800

    TFS-YX:02 日志配置

--- src/test/java/com/imooc/LoggerTest.java
+++ src/test/java/com/imooc/LoggerTest.java
@@ -1,23 +1,28 @@
 package com.imooc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LoggerTest {
 
-    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+// private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+@Slf4j
+public class LoggerTest {
 
     @Test
     public void test1() {
-        logger.debug("debug...");
-        logger.info("info...");
-        logger.error("error...");
+        String name = "daozhou";
+        String passWord = "123456";
+        log.info("name: {}, password: {}", name, passWord);
+
+        log.debug("debug...");
+        log.info("info...");
+        log.error("error...");
+        log.warn("warn...");
     }
 
 }
